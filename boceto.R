2a760c6b1ae17aad603546aa463e3b526af07e8e
bastianolea <bastianolea@gmail.com>
2025-06-17 23:17:02 -0400

    boceto

---
+++ boceto.R
@@ -0,0 +1,21 @@
+
+# series de datos a considerar
+
+# economía ----
+# aumento sostenido del pib
+# aumento de la inversión extranjera
+# reducción de la inflación
+# ipsa histórico
+
+# delincuencia ----
+# delitos dentro del rango pre pandemia
+# disminución de la tendencia al alza de los homicidios
+# leyes sobre delincuencia
+
+# migración ----
+# baja de la ola migratoria de 2021-2022
+
+# energía ----
+# aumento reciente de energía solar
+# referente internacional de energía solar
+# buses eléctricos
